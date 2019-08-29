70abefe42db251bd4eff963574ff8a9c561b423a
wk <1213228398@qq.com>
2019-08-29 17:38:21 +0800

    把idea中的工程添加到git

---
+++ git-test-project/src/com/wk/git/FirstGit.java
@@ -0,0 +1,15 @@
+package com.wk.git;
+
+/**
+ * Author:kai
+ * Date：2019­08­29 17:29  
+ * Description：<描述>
+ */
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
