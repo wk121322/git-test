1523c860f08b031aa78552ad77a2d71fac0c2eea
wk <1213228398@qq.com>
2019-08-29 22:01:34 +0800

    修改firstGit

--- git-test-project/src/com/wk/git/FirstGit.java
+++ git-test-project/src/com/wk/git/FirstGit.java
@@ -11,6 +11,7 @@ public class FirstGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hello
     }
 
     public void test(){
